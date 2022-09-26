449b44a04b75f36e10dae6f9e447084999dbbc08
jinjahninjah <royce.j.roberts@gmail.com>
2022-09-25 20:12:12 -0500

    Adds SQL database implementation

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+Base = declarative_base()
+engine = create_engine(
+    url=os.getenv("SQL_DB_URL"),
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
---
+++ table_models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String, Column
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    profile_id = Column(
+        String,
+        primary_key=True,
+        unique=True,
+        nullable=False,
+        index=True,
+    )
+    name = Column(String, index=True)
+    email = Column(String, index=True)
+
